1934bab02bc62b42b935e9218d2ed07761f6a8d2
lert707 <lert707@163.com>
2018-10-15 11:38:28 +0800

    update

--- www/src/router/index.js
+++ www/src/router/index.js
@@ -6,7 +6,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', name: 'index', component: () => import ('@/components/index/index') },
-    { path: '/home', name: 'home', component: () => import ('@/components/home/home') }
+    { path: '/', name: 'index', component: () => import ('@/components/index/index') }
   ]
 })
